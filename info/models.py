389d72b66843a2deb76272bda949252535ce5a04
samugd17 <samugd17@gmail.com>
2023-11-26 22:43:42 +0000

    first instance for models

--- info/models.py
+++ info/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Competitor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=60, blank=True)
+    birthdate = models.DateField(auto_now=False)
+    # subject
+    city = models.CharField(max_length=30)
+    # job
+    hobbies = models.TextField()
+    avatar = models.ImageField(upload_to='competitors/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=60, blank=True)
+    #subject
+    avatar = models.ImageField(upload_to='teachers/%Y/%m/%d/', blank=True)
+
+class Judge(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=60, blank=True)
+    #job
+    avatar = models.ImageField(upload_to='judges/%Y/%m/%d/', blank=True)    
--- ot/settings.py
+++ ot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "info.apps.InfoConfig",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media Files configuration
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
