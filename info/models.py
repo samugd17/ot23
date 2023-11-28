47a9e53a3c6931b07282ea8b6873b28ff5abf419
samugd17 <samugd17@gmail.com>
2023-11-28 15:36:32 +0000

    models done

--- info/models.py
+++ info/models.py
@@ -1,29 +1,49 @@
 from django.db import models
 
+
+class MusicStyle(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, blank=True)
+
+
 class Competitor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     slug = models.SlugField(max_length=60, blank=True)
     birthdate = models.DateField(auto_now=False)
-    # subject
     city = models.CharField(max_length=30)
-    # job
+    job = models.CharField(max_length=30, blank=True)
     hobbies = models.TextField()
+    style = models.ManyToManyField(MusicStyle)
     avatar = models.ImageField(upload_to='competitors/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
+
 class Teacher(models.Model):
+    class Subjects(models.TextChoices):
+        REVIEW = "REW", "Review"
+        CHOREOGRAPHY = "CHP", "Choreography"
+        FITNESS = "FIT", "Fitness"
+        YOGA = "YOG", "Yoga"
+        MUSICAL_DIRECTOR = "MDR", "Musical Director"
+        VOCAL_TECHNIQUE = "VTC", "Vocal technique"
+        URBAN_DANCE = "UDC", "Urban Dance"
+        ENGLISH = "ENG", "English"
+        CHORAL_SINGING = "CSG", "Choral Singing"
+        INTERPRETATION = "INT", "Interpretation"
+
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     slug = models.SlugField(max_length=60, blank=True)
-    #subject
+    subject = models.CharField(max_length=3, choices=Subjects.choices, default="")
     avatar = models.ImageField(upload_to='teachers/%Y/%m/%d/', blank=True)
+
 
 class Judge(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     slug = models.SlugField(max_length=60, blank=True)
-    #job
-    avatar = models.ImageField(upload_to='judges/%Y/%m/%d/', blank=True)    
+    job = models.CharField(max_length=30, blank=True)
+    avatar = models.ImageField(upload_to='judge/%Y/%m/%d/', blank=True)
