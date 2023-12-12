a5159690cde225d342e5e82513c4f8ec23b3ad26
samugd17 <samugd17@gmail.com>
2023-12-12 13:55:01 +0000

    breadcrumbs added

--- info/models.py
+++ info/models.py
@@ -23,8 +23,9 @@ class Competitor(models.Model):
     style = models.ManyToManyField(MusicStyle)
     avatar = models.ImageField(upload_to='competitors/%Y/%m/%d/', blank=True)
 
-    def __str__(self):
-        return self.first_name
+    @property
+    def fullname(self):
+        return f'{self.first_name} {self.last_name}'
 
     def get_absolute_url(self):
         return reverse("info:competitor_detail", kwargs={"slug": self.slug})
@@ -63,6 +64,10 @@ class Judge(models.Model):
     slug = models.SlugField(max_length=60, blank=True)
     job = models.CharField(max_length=120)
     avatar = models.ImageField(upload_to='judge/%Y/%m/%d/', blank=True)
+
+    @property
+    def fullname(self):
+        return f'{self.first_name} {self.last_name}'
 
     def get_absolute_url(self):
         return reverse("info:judge_detail", kwargs={"slug": self.slug})
--- info/views.py
+++ info/views.py
@@ -1,41 +1,58 @@
-from django.shortcuts import get_object_or_404, render
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, render
 
 from .models import Competitor, Judge, Teacher
 
 
 def teacher_list(request):
     teachers = Teacher.objects.all()
-    return render(request, 'info/teacher/list.html', {'teachers': teachers})
+    return render(request, 'info/teacher/list.html', dict(teachers=teachers, section='Profesores'))
 
 
 def teacher_detail(request, slug):
     teacher = get_object_or_404(Teacher, slug=slug)
-    return render(request, 'info/teacher/detail.html', {'teacher': teacher})
+    return render(
+        request,
+        'info/teacher/detail.html',
+        dict(teacher=teacher, section=f'Profesores / {teacher.fullname}'),
+    )
 
 
 def judge_list(request):
     judges = Judge.objects.all()
-    return render(request, 'info/judge/list.html', {'judges': judges})
+    return render(request, 'info/judge/list.html', dict(judges=judges, section='Jurado'))
 
 
 def judge_detail(request, slug):
     judge = get_object_or_404(Judge, slug=slug)
-    return render(request, 'info/judge/detail.html', {'judge': judge})
+    return render(
+        request, 'info/judge/detail.html', dict(judge=judge, section=f'Jurado / {judge.fullname}')
+    )
 
 
 def competitor_list(request):
     competitors = Competitor.objects.all()
-    return render(request, 'info/competitor/list.html', {'competitors': competitors})
+    return render(
+        request, 'info/competitor/list.html', dict(competitors=competitors, section='Alumnos')
+    )
 
 
 def competitor_detail(request, slug):
     competitor = get_object_or_404(Competitor, slug=slug)
-    return render(request, 'info/competitor/detail.html', {'competitor': competitor})
+    return render(
+        request,
+        'info/competitor/detail.html',
+        dict(competitor=competitor, section=f'Alumnos / {competitor.fullname}'),
+    )
 
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def breadcrumbs(request):
+    sections = ['Alumnos', 'Profesores', 'Jurado']
+    return render(request, 'info/breadcrumbs.html', {'sections': sections})
 
 
 # def search(request, tofind: str):
