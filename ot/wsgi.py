a202c525dc226261f11504169d429d9cb3834193
Samuel González <samuglezdiaz@gmail.com>
2025-05-12 11:41:34 +0100

    forzando redeploy

--- ot/wsgi.py
+++ ot/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+import traceback
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ot.settings")
 
-application = get_wsgi_application()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ot.settings')
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    sys.stderr.write("ERROR al iniciar Django:\n")
+    traceback.print_exc()
+
