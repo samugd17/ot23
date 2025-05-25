a337906aa1fb17c1622297af4184979a86334452
Samuel González <samuglezdiaz@gmail.com>
2025-05-25 19:56:42 +0100

    entrypoint edited

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,13 @@
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
+echo "Preparing staticfiles folder..."
+mkdir -p /app/staticfiles
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
 gunicorn ot.wsgi:application --bind 0.0.0.0:8080
+
 
